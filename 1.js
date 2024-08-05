c3efc1d8bbc6c6c2b32d5aecffd982f2becd353b
coderRitikPro <ritikorkumar@gmail.com>
2024-08-05 09:25:04 +0530

    modified

--- 1.js
+++ 1.js
@@ -12,6 +12,5 @@ var animateButton = function(e) {
 
 var bubblyButtons = document.getElementsByClassName("bubbly-button");
 
-for (var i = 0; i < bubblyButtons.length; i++) {
-  bubblyButtons[i].addEventListener('click', animateButton, false);
-}
+
+  bubblyButtons.addEventListener('click', animateButton, false);
